ab8f24b810a63a19bcbd4fb55d5f4aa9e18f7170
Vishal Sir <rajat@Rajats-MacBook-Air.local>
2023-09-02 19:17:26 +0530

    fix: ignore

--- Components/index.js
+++ Components/index.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import UserList from "./UserList/UserList";
+
 import axios from "axios";
 const ComponentIndex = () => {
   const [renterData, setRenterData] = useState([]);
